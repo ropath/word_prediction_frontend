b2dbaa913c5fe87002e80d85d8b6c0afc7b146e4
Robert <robertweingarten@web.de>
2024-11-20 22:47:27 +0100

    Update word_prediction_app.py

--- word_prediction_app.py
+++ word_prediction_app.py
@@ -22,7 +22,7 @@ def main():
 
     if run:
         # Open the webcam using the provided stream URI
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(1)
         if not cap.isOpened():
             st.error("Error: Could not open video stream.")
             return
