f579315db1d47c242899110fbbce08363928ee7c
Robert <robertweingarten@web.de>
2024-11-21 10:52:34 +0100

    Update word_prediction_app.py

--- word_prediction_app.py
+++ word_prediction_app.py
@@ -66,7 +66,7 @@ def main():
             # Display the frame in Streamlit
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             img_pil = Image.fromarray(frame_rgb)
-            placeholder.image(img_pil, use_container_width=True)
+            placeholder.image(img_pil)
 
             # Update the progress bar while collecting frames for prediction
             progress = min(progress + 1, total_frames)
